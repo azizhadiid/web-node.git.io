93dd6ae6603aaef5ec7ac3fbcf9e5b388fb4f4c1
azizhadiid <azizalhadiid88@mgail.com>
2025-01-08 07:17:15 +0700

    Express View Engine

--- app.js
+++ app.js
@@ -1,36 +1,59 @@
 const express = require('express');
+const expressLayouts = require('express-ejs-layouts');
 const app = express();
 const port = 3000;
 
+// Gunakan EJS
+app.set('view engine', 'ejs');
+app.use(expressLayouts);
+
 // root
 app.get('/', (req, res) => {
-   //   res.send('Hello World!')
+  const mahasiswa = [{
+      nama: 'Aziz Alhadiid',
+      email: 'aziz@gmail.com',
+    },
+    {
+      nama: 'Agus Bagus',
+      email: 'agus@gmail.com'
+    }
+  ];
 
-    // jika ingin mengambil file HTML
-    res.sendFile('./index.html', {root: __dirname});
+  res.render('index', {
+    layout: 'layouts/main-layout',
+    nama: 'Aziz Alhadiid',
+    title: 'Ini adalah Home',
+    mahasiswa,
+  });
 });
 
 // ini adalah halaman about
 app.get('/about', (req, res) => {
-    res.sendFile('./about.html', {root: __dirname});
+  res.render('about', {
+    layout: 'layouts/main-layout',
+    title: 'Halaman About'
+  });
 });
 
 // ini adalah halaman contact
 app.get('/contact', (req, res) => {
-    res.sendFile('./contact.html', {root: __dirname});
+  res.render('contact', {
+    layout: 'layouts/main-layout',
+    title: 'Halaman Contact'
+  });
 });
 
 // ini ada params
 app.get('/product/:id', (req, res) => {
-    res.send(`Product ID: ${req.params.id} <br> Category : ${req.query.category}`);
+  res.send(`Product ID: ${req.params.id} <br> Category : ${req.query.category}`);
 });
 
 // Simpan ini paling bawah
 app.use('/', (req, res) => {
-    res.status(404);
-    res.send('<h1>404</h1>');
+  res.status(404);
+  res.send('<h1>404</h1>');
 });
 
 app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Example app listening on port ${port}`)
+});
